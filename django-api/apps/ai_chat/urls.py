29425951593dc00b019bba0bfa9e677f48d566af
eranmoss-Bridgify <eranm@bridgify.io>
2026-06-12 09:35:49 -0400

    Move django-api to tos-hub repo as trip-planner-api (layer separation)

--- django-api/apps/ai_chat/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('chat', views.AiChatView.as_view()),
-    path('day-title', views.DayTitleView.as_view()),
-]
--- django-api/trip_planner_api/wsgi.py
+++
@@ -1,4 +0,0 @@
-import os
-from django.core.wsgi import get_wsgi_application
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'trip_planner_api.settings')
-application = get_wsgi_application()
